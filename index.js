fcf28cb52a34e9b447e695f76f7ecb9642668ce2
Sina Moradian <sinacsharp@gmail.com>
2022-05-16 00:42:46 +0430

    Add brightness, contrast, grayscale and reset

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 SELECTORS
 -------*/
 const controls = document.querySelectorAll('.controls input');
+const resetBtn = document.querySelector('.reset-btn');
 const pic = document.querySelector(".pic");
+const inputs = document.querySelectorAll("input");
+
 
 /*-----
 EVENT LISTENERS
@@ -11,6 +14,8 @@ controls.forEach(input => {
     input.addEventListener('input', handleUpdate);
 });
 
+resetBtn.addEventListener("click", resetProperties);
+
 /*-----
 Functions
 -------*/
@@ -18,4 +23,38 @@ function handleUpdate(e) {
     // dataset will give you an object of data- attributes 
     const suffix = this.dataset.sizing || ""; //to prevent undefind value and also give px at the end of styles.
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix); // change the style of root element(entire document) 
+}
+
+function resetProperties(e) {
+    document.documentElement.style.setProperty("--blur", 0);
+    document.documentElement.style.setProperty("--base", "rgb(161, 200, 255)");
+    document.documentElement.style.setProperty("--spacing", "10px");
+    document.documentElement.style.setProperty("--brightness", "100%");
+    document.documentElement.style.setProperty("--contrast", "100%");
+    document.documentElement.style.setProperty("--grayscale", 0);
+
+    const allinputs = [...inputs];
+    allinputs.forEach(input => {
+        switch (input.name) {
+            case "spacing":
+                input.value = 10;
+                break;
+            case "blur":
+                input.value = 0;
+                break;
+            case "base":
+                input.value = "rgb(161, 200, 255)";
+                break;
+            case "brightness":
+                input.value = 100;
+                break;
+            case "contrast":
+                input.value = 100;
+                break;
+            case "grayscale":
+                input.value = 0;
+                break;
+        }
+    });
+
 }
